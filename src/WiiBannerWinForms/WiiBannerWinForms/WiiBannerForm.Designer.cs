b9b5c87e4be1fc6d905413e73bb1eebf4aabe699
Sarkie <Sarkie@Gmail.com>
2015-02-02 15:08:43 +0000

    copied tools

---
+++ src/WiiBannerWinForms/WiiBannerWinForms/WiiBannerForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WiiBannerWinForms
+{
+    partial class WiiBannerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.BannersListBox = new System.Windows.Forms.ListBox();
+            this.LoadBannersButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(149, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Wii Banner Example App";
+            // 
+            // BannersListBox
+            // 
+            this.BannersListBox.FormattingEnabled = true;
+            this.BannersListBox.Location = new System.Drawing.Point(154, 53);
+            this.BannersListBox.Name = "BannersListBox";
+            this.BannersListBox.Size = new System.Drawing.Size(245, 277);
+            this.BannersListBox.TabIndex = 1;
+            // 
+            // LoadBannersButton
+            // 
+            this.LoadBannersButton.Location = new System.Drawing.Point(154, 355);
+            this.LoadBannersButton.Name = "LoadBannersButton";
+            this.LoadBannersButton.Size = new System.Drawing.Size(245, 44);
+            this.LoadBannersButton.TabIndex = 2;
+            this.LoadBannersButton.Text = "Load Banners";
+            this.LoadBannersButton.UseVisualStyleBackColor = true;
+            this.LoadBannersButton.Click += new System.EventHandler(this.LoadBannersButton_Click);
+            // 
+            // WiiBannerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 427);
+            this.Controls.Add(this.LoadBannersButton);
+            this.Controls.Add(this.BannersListBox);
+            this.Controls.Add(this.label1);
+            this.Name = "WiiBannerForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox BannersListBox;
+        private System.Windows.Forms.Button LoadBannersButton;
+    }
+}
+
---
+++ src/WiiBannerWinForms/WiiBannerWinForms/WiiBannerForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WiiBannerWinForms
+{
+    public partial class WiiBannerForm : Form
+    {
+        public WiiBannerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadBannersButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.SelectedPath = Path.GetDirectoryName(@"C:\Work\gitroot\dolphin\WiiBanner\games\");
+
+            DialogResult result = fbd.ShowDialog();
+
+            string[] folders = Directory.GetDirectories(fbd.SelectedPath);
+
+            foreach (var folder in folders)
+            {
+                DirectoryInfo di = new DirectoryInfo(folder);
+
+                switch (di.Name)
+                {
+                    case "SMNP01":
+                        BannersListBox.Items.Add(di.Name);
+
+                        var location = "files/EU/save_banner.arc";
+
+                        ExtractARC(folder + location, "./banners/"+di.Name);
+
+
+
+                        break;
+
+                    case "STUPRN":
+                        BannersListBox.Items.Add(di.Name);
+
+                        break;
+
+                    default:
+                        BannersListBox.Items.Add("Do not have banner config for game:" + di.Name);
+                        break;
+                }
+            }
+        }
+
+        private void ExtractARC(string arcPath, string savePath)
+        {
+            //"wszst.ext extract %path%"
+
+            // move contents
+        }
+
+        private void CallWiimmsSZSTool()
+        {
+
+        }
+
+
+        private void ConvertTPLToPNG(string tplPath, string pngPath)
+        {
+
+        }
+
+        private void ConvertBTIToPNG(string btiPath, string pngBath)
+        {
+            // wimgt.exe decode btipath 
+            // will do decoding in the path .png
+        }
+
+        private void CallWiimmsImageTool(string decodePath)
+        {
+
+        }
+    }
+}
